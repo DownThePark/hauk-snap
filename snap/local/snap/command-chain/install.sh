be8395450b83a60bc1c684ea52ed0afcda590a4d
DownThePark <28867175+DownThePark@users.noreply.github.com>
2024-10-22 19:23:23 -0400

    Fix copying config.php during install hook

--- snap/local/snap/command-chain/install.sh
+++ snap/local/snap/command-chain/install.sh
@@ -4,7 +4,7 @@
 initialize_snap_data() {
   # Configuration
   mkdir $SNAP_DATA/etc
-  cp $SNAP/etc/config.php $SNAP_DATA/etc/
+  cp $SNAP/html/include/config-sample.php $SNAP_DATA/etc/config.php
 
   # Logs
   mkdir $SNAP_DATA/log
