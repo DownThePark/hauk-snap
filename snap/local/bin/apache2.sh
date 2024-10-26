be10dbdfc52b1940d0e9956c58110393e022bdb6
DownThePark <28867175+DownThePark@users.noreply.github.com>
2024-10-25 21:36:06 -0400

    Validate HTTP port value

--- snap/local/bin/apache2.sh
+++ snap/local/bin/apache2.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-source $SNAP_DATA/etc/server.conf
-
 export APACHE_RUN_USER=snap_daemon
 export APACHE_RUN_GROUP=snap_daemon
 export APACHE_RUN_DIR=/var/apache2/run
 export APACHE_LOCK_DIR=/var/apache2/lock
 export APACHE_LOG_DIR=/var/apache2/log
 export APACHE_PID_FILE=/var/apache2/run/apache2.pid
+
+source $SNAP_DATA/etc/server.conf
+
+if [ -z "$HTTP_PORT" ] ; then
+  echo "Error: The HTTP_PORT value seems to be missing in $SNAP_DATA/etc/server.conf" >&2
+  exit 1
+fi
+
+if ! expr "$HTTP_PORT" : '^[0-9]\+$' > /dev/null ; then
+  echo "Error: \"$HTTP_PORT\" is not a valid HTTP port" >&2
+  exit 1
+fi
 
 if [ ! -d /var/apache2 ] ; then
   mkdir /var/apache2
