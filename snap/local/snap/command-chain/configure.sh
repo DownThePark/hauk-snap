c8ad3f4211cd0f403f30b78e1f2a6740613b6a52
DownThePark <28867175+DownThePark@users.noreply.github.com>
2024-10-28 10:22:48 -0400

    Check if SSL configuration is valid

--- snap/local/snap/command-chain/configure.sh
+++ snap/local/snap/command-chain/configure.sh
@@ -10,7 +10,7 @@ SSL_CERT="$(snapctl get ssl.cert)"
 SSL_KEY="$(snapctl get ssl.key)"
 
 configure_apache2() {
-  # Check if HTTP port values are valid or not
+  # Check if HTTP port value is valid or not
   if ! expr "$HTTP_PORT" : '^[0-9]\+$' > /dev/null ; then
     echo "Error: \"$HTTP_PORT\" is not a numerical value" >&2
     exit 1
@@ -32,11 +32,23 @@ configure_apache2() {
 
   # Enable or disable HTTPS
   if [[ $SSL_ENABLED == 'true' ]] ; then
-    $SNAP/bin/apache2 -t enable
+    $SNAP/bin/apache2.sh -t enable
+  elif [[ $SSL_ENABLED == 'false' ]] ; then
+    $SNAP/bin/apache2.sh -t disable
+  else
+    echo "Error: \"$SSL_ENABLED\" is an invalid value. It must be set to either true or false."
+    exit 1
   fi
 
-  if [[ $SSL_ENABLED == 'false' ]] ; then
-    $SNAP/bin/apache2 -t disable
+  # Check if certificate paths are valid
+  if [[ ! -f $SSL_CERT ]] ; then
+    echo "Error: \"$SSL_CERT\" is an invalid path."
+    exit 1
+  fi
+
+  if [[ ! -f $SSL_KEY ]] ; then
+    echo "Error: \"$SSL_KEY\" is an invalid path."
+    exit 1
   fi
 
   snapctl set ports.http=$HTTP_PORT
