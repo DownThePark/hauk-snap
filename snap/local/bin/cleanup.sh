9bf8686088bab52ff2455155d62eb23e80efbe9a
DownThePark <28867175+DownThePark@users.noreply.github.com>
2024-10-28 15:45:11 -0400

    Use environment variable to retrieve system arch

--- snap/local/bin/cleanup.sh
+++ snap/local/bin/cleanup.sh
@@ -2,10 +2,10 @@
 
 rm -r lib
 rm -r usr/include
-rm -r lib/x86_64-linux-gnu/perl
-rm -r lib/x86_64-linux-gnu/perl-base
-rm -r usr/lib/x86_64-linux-gnu/perl
-rm -r usr/lib/x86_64-linux-gnu/perl-base
+rm -r lib/$CRAFT_ARCH_TRIPLET_BUILD_FOR/perl
+rm -r lib/$CRAFT_ARCH_TRIPLET_BUILD_FOR/perl-base
+rm -r usr/lib/$CRAFT_ARCH_TRIPLET_BUILD_FOR/perl
+rm -r usr/lib/$CRAFT_ARCH_TRIPLET_BUILD_FOR/perl-base
 rm -r usr/share/doc
 rm -r usr/share/man
 rm -r usr/share/perl
