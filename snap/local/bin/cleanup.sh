fa356afa93ebc0e2fea6e34789d3adecd71c64a4
DownThePark <28867175+DownThePark@users.noreply.github.com>
2024-10-28 15:43:07 -0400

    Remove unused directories
    This significantly helps save disk space

---
+++ snap/local/bin/cleanup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -r lib
+rm -r usr/include
+rm -r lib/x86_64-linux-gnu/perl
+rm -r lib/x86_64-linux-gnu/perl-base
+rm -r usr/lib/x86_64-linux-gnu/perl
+rm -r usr/lib/x86_64-linux-gnu/perl-base
+rm -r usr/share/doc
+rm -r usr/share/man
+rm -r usr/share/perl
+rm -r var
