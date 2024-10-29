2e7b18d05b8de3a3d8332d014c6008912e15bdf4
DownThePark <28867175+DownThePark@users.noreply.github.com>
2024-10-29 12:27:26 -0400

    Move memcached.sock to /tmp/memcached/run

--- snap/local/bin/memcached.sh
+++ snap/local/bin/memcached.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-$SNAP/usr/bin/memcached -s $SNAP_DATA/run/memcached.sock -m 64 -u root
+export MEMCACHED_RUN_DIR=/tmp/memcached/run
+
+if [ ! -d $MEMCACHED_RUN_DIR ] ; then
+  mkdir -p $MEMCACHED_RUN_DIR
+fi
+
+$SNAP/usr/bin/memcached -s $MEMCACHED_RUN_DIR/memcached.sock -m 64 -u root
--- snap/local/snap/command-chain/install.sh
+++ snap/local/snap/command-chain/install.sh
@@ -15,7 +15,6 @@ initialize_snap_configuration() {
 initialize_snap_data() {
   # Create folders
   mkdir $SNAP_DATA/etc
-  mkdir $SNAP_DATA/run
   mkdir $SNAP_DATA/ssl
 
   # config.php
